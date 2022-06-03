a6035f0e5d7ba23f4c3d49403ac82e8f8e96dc09
tishaani <335762183@gapps.yrdsb.ca>
2022-06-03 09:04:51 -0400

    Creating mouse for maze

---
+++ MazeRunner.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MazeRunner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MazeRunner extends Actor
+{
+    /**
+     * Act - do whatever the MazeRunner wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    //Create contructor method
+    public MazeRunner()
+    {
+        getImage().scale(getImage().getWidth()/2,getImage().getHeight()/2);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- MazeWorld.java
+++ MazeWorld.java
@@ -16,6 +16,109 @@ public class MazeWorld extends World
     public MazeWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 600, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        MazeBlock mazeBlock = new MazeBlock();
+        addObject(mazeBlock,0,10*50);
+        MazeBlock mazeBlock2 = new MazeBlock();
+        addObject(mazeBlock2,0,9*50);
+        MazeBlock mazeBlock3 = new MazeBlock();
+        addObject(mazeBlock3,0,8*50);
+        MazeBlock mazeBlock4 = new MazeBlock();
+        addObject(mazeBlock4,2*50,11*50);
+        MazeBlock mazeBlock5 = new MazeBlock();
+        addObject(mazeBlock5,2*50,10*50);
+        MazeBlock mazeBlock6 = new MazeBlock();
+        addObject(mazeBlock6,1*50,8*50);
+        MazeBlock mazeBlock7 = new MazeBlock();
+        addObject(mazeBlock7,2*50,8*50);
+        MazeBlock mazeBlock8 = new MazeBlock();
+        addObject(mazeBlock8,3*50,8*50);
+        MazeBlock mazeBlock9 = new MazeBlock();
+        addObject(mazeBlock9,4*50,8*50);
+        MazeBlock mazeBlock10 = new MazeBlock();
+        addObject(mazeBlock10,4*50,10*50);
+        MazeBlock mazeBlock11 = new MazeBlock();
+        addObject(mazeBlock11,6*50,11*50);
+        MazeBlock mazeBlock12 = new MazeBlock();
+        addObject(mazeBlock12,6*50,10*50);
+        MazeBlock mazeBlock13 = new MazeBlock();
+        addObject(mazeBlock13,6*50,9*50);
+        MazeBlock mazeBlock14 = new MazeBlock();
+        addObject(mazeBlock14,6*50,8*50);
+        MazeBlock mazeBlock15 = new MazeBlock();
+        addObject(mazeBlock15,6*50,7*50);
+        MazeBlock mazeBlock16 = new MazeBlock();
+        addObject(mazeBlock16,6*50,6*50);
+        MazeBlock mazeBlock17 = new MazeBlock();
+        addObject(mazeBlock17,5*50,6*50);
+        MazeBlock mazeBlock18 = new MazeBlock();
+        addObject(mazeBlock18,5,5);
+        MazeBlock mazeBlock19 = new MazeBlock();
+        addObject(mazeBlock19,5*50,4*50);
+        MazeBlock mazeBlock20 = new MazeBlock();
+        addObject(mazeBlock20,5*50,3*50);
+        MazeBlock mazeBlock21 = new MazeBlock();
+        addObject(mazeBlock21,2*50,6*50);
+        mazeBlock21.setLocation(2,6);
+        MazeBlock mazeBlock22 = new MazeBlock();
+        addObject(mazeBlock22,2*50,5*50);
+        MazeBlock mazeBlock23 = new MazeBlock();
+        addObject(mazeBlock23,2*50,3*50);
+        MazeBlock mazeBlock24 = new MazeBlock();
+        addObject(mazeBlock24,2*50,2*50);
+        MazeBlock mazeBlock25 = new MazeBlock();
+        addObject(mazeBlock25,1*50,2*50);
+        MazeBlock mazeBlock26 = new MazeBlock();
+        addObject(mazeBlock26,3*50,1*50);
+        MazeBlock mazeBlock27 = new MazeBlock();
+        addObject(mazeBlock27,4*50,1*50);
+        MazeBlock mazeBlock28 = new MazeBlock();
+        addObject(mazeBlock28,5*50,1*50);
+        MazeBlock mazeBlock29 = new MazeBlock();
+        addObject(mazeBlock29,6*50,2*50);
+        MazeBlock mazeBlock30 = new MazeBlock();
+        addObject(mazeBlock30,0,0);
+        MazeBlock mazeBlock31 = new MazeBlock();
+        addObject(mazeBlock31,7*50,2*50);
+        MazeBlock mazeBlock32 = new MazeBlock();
+        addObject(mazeBlock32,8*50,0);
+        MazeBlock mazeBlock33 = new MazeBlock();
+        addObject(mazeBlock33,9*50,1*50);
+        MazeBlock mazeBlock34 = new MazeBlock();
+        addObject(mazeBlock34,10*50,2*50);
+        MazeBlock mazeBlock35 = new MazeBlock();
+        addObject(mazeBlock35,10*50,3*50);
+        MazeBlock mazeBlock36 = new MazeBlock();
+        addObject(mazeBlock36,9*50,4*50);
+        MazeBlock mazeBlock37 = new MazeBlock();
+        addObject(mazeBlock37,8*50,4*50);
+        MazeBlock mazeBlock38 = new MazeBlock();
+        addObject(mazeBlock38,7*50,6*50);
+        MazeBlock mazeBlock39 = new MazeBlock();
+        addObject(mazeBlock39,10*50,5*50);
+        MazeBlock mazeBlock40 = new MazeBlock();
+        addObject(mazeBlock40,10*50,6*50);
+        MazeBlock mazeBlock41 = new MazeBlock();
+        addObject(mazeBlock41,10*50,7*50);
+        MazeBlock mazeBlock42 = new MazeBlock();
+        addObject(mazeBlock42,10*50,8*50);
+        MazeBlock mazeBlock43 = new MazeBlock();
+        addObject(mazeBlock43,9*50,6*50);
+        MazeBlock mazeBlock44 = new MazeBlock();
+        addObject(mazeBlock44,9*50,9*50);
+        MazeBlock mazeBlock45 = new MazeBlock();
+        addObject(mazeBlock45,8*50,9*50);
+        MazeBlock mazeBlock46 = new MazeBlock();
+        addObject(mazeBlock46,8*50,10*50);
+        MazeBlock mazeBlock47 = new MazeBlock();
+        addObject(mazeBlock47,10*50,11*50);
     }
 }
