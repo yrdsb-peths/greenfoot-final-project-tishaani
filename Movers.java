39862f0c698dfae2b3297405200e6b8429f98507
tishaani <335762183@gapps.yrdsb.ca>
2022-06-06 08:47:55 -0400

    Created movers class

---
+++ Movers.java
@@ -0,0 +1,44 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Movers here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Movers extends Actor
+{
+    /**
+     * Act - do whatever the Movers wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        
+    }
+    public void moveAndTurn()
+    {
+        if (Greenfoot.isKeyDown("right"))
+    {
+        setRotation(0);
+        move(4);
+    }
+    if (Greenfoot.isKeyDown("left"))
+    {
+        setRotation(180);
+        move(4);
+    }
+    if(Greenfoot.isKeyDown("up"))
+    {
+        setRotation(270);
+        move(4);
+    }
+    if(Greenfoot.isKeyDown("down"))
+    {
+        setRotation(90);
+        move(4);
+    }
+    }
+}
+
