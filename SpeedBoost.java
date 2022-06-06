163e508412bd70dab0e1065e1c012794cc490525
tishaani <335762183@gapps.yrdsb.ca>
2022-06-06 09:10:19 -0400

    Added the boost for mazerunner

---
+++ SpeedBoost.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SpeedBoost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SpeedBoost extends Actor
+{
+    /**
+     * Act - do whatever the SpeedBoost wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public SpeedBoost()
+    {
+        getImage().scale(getImage().getWidth()/2,getImage().getHeight()/2);
+    }
+    public void act()
+    {
+        // Add your action code here.
+        
+    }
+}
