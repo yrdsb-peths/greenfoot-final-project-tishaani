96d80f523fa453e684a927957459cf70c8aa866b
tishaani <335762183@gapps.yrdsb.ca>
2022-06-14 19:16:30 -0400

    Editing class

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    GreenfootSound backgroundMusic = new GreenfootSound("backgroundMusic.mp3");
+    //GreenfootSound backgroundMusic = new GreenfootSound("backgroundMusic.mp3");
+
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,12 +18,12 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        backgroundMusic.playLoop();
-        addObject(new moveKeys(), 300,300);
+        //backgroundMusic.playLoop();
+        addObject(new MoveKeys(), 300,300);
 
         prepare();
     }
-
+    
     public void act()
     {
         //start the game if the user presses space bar 
@@ -39,12 +40,12 @@ public class MyWorld extends World
      */
     private void prepare()
     {
-        pressStart pressStart = new pressStart();
+        PressStart pressStart = new PressStart();
         addObject(pressStart,288,120);
         pressStart.setLocation(300,200);
-        welcomeLabel welcomeLabel = new welcomeLabel();
+        WelcomeLabel welcomeLabel = new WelcomeLabel();
         addObject(welcomeLabel,298,77);
-        goalGame goalGame = new goalGame();
+        GoalGame goalGame = new GoalGame();
         addObject(goalGame,298,359);
         goalGame.setLocation(296,355);
     }
